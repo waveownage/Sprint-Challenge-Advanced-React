b3480b3d1083efbdc8d2f24539d2d99c42271c3e
Andre Freeman <asf@mail.usf.edu>
2020-04-19 20:35:56 -0400

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {},
+      followers: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/api/players`)
+      .then(res => {
+        this.setState({ user: res.data });
+        console.log(res.data);
+      })
+      .catch(err => console.log("ERROR!", err));
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hi!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
